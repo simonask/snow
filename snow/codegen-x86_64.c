72ded3019434e63e1e6dea3beaa4896e2cbee723
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-07-11 23:24:27 +0200

    x86_64 codegen: asm convenience, basic return support

--- snow/codegen-x86_64.c
+++ snow/codegen-x86_64.c
@@ -2,17 +2,21 @@
 #include "snow/intern.h"
 #include "snow/linkbuffer.h"
 #include "snow/asm-x86_64.h"
+#include "snow/array.h"
 #include "snow/gc.h"
 
 #include <sys/mman.h>
 
+
 typedef struct SnCodegenX {
 	SnCodegen base;
 	SnLinkBuffer* buffer;
+	SnLinkBuffer* returns;
 	uintx num_temporaries; // etc.
 } SnCodegenX;
 
 static void codegen_free(VALUE);
+static void codegen_compile_root(SnCodegenX* cgx);
 static void codegen_compile_node(SnCodegenX* cgx, SnAstNode*);
 
 SnCodegen* snow_create_codegen(SnAstNode* root)
@@ -22,6 +26,7 @@ SnCodegen* snow_create_codegen(SnAstNode* root)
 	snow_gc_set_free_func(codegen, codegen_free);
 	codegen->base.root = root;
 	codegen->buffer = snow_create_linkbuffer(1024);
+	codegen->returns = NULL;
 	codegen->num_temporaries = 0;
 	return (SnCodegen*)codegen;
 }
@@ -35,7 +40,8 @@ static void codegen_free(VALUE cg)
 SnFunction* snow_codegen_compile(SnCodegen* cg)
 {
 	SnCodegenX* cgx = (SnCodegenX*)cg;
-	codegen_compile_node(cgx, cgx->base.root);
+	
+	codegen_compile_root(cgx);
 	
 	uintx len = snow_linkbuffer_size(cgx->buffer);
 	byte* compiled_code = (byte*)valloc(len);
@@ -49,20 +55,94 @@ SnFunction* snow_codegen_compile(SnCodegen* cg)
 	return func;
 }
 
+#define ASM(instr, ...) asm_##instr(cgx->buffer, ##__VA_ARGS__)
+#define CURRENT_OFFSET() (offset)snow_linkbuffer_size(cgx->buffer)
+#define WRITE_OFFSET(src, dst) 
+
+void codegen_compile_root(SnCodegenX* cgx)
+{
+	ASSERT(cgx->base.root->type == SN_AST_FUNCTION);
+	
+	// compile function prelude/postlude
+	
+	codegen_compile_node(cgx, cgx->base.root);
+	
+	Label return_label = ASM(label);
+	ASM(bind, &return_label);
+	
+	// link all returns
+	if (cgx->returns) {
+		uintx bytes = snow_linkbuffer_size(cgx->returns);
+		uintx len = bytes / sizeof(int32_t);
+		int32_t offsets[bytes];
+		snow_linkbuffer_copy_data(cgx->returns, offsets, bytes);
+		
+		for (uintx i = 0; i < len; ++i) {
+			LabelRef ref;
+			ref.offset = offsets[i];
+			ref.label = &return_label;
+			ASM(link, &ref);
+		}
+	}
+}
+
 void codegen_compile_node(SnCodegenX* cgx, SnAstNode* node)
 {
+	ASSERT(node->base.type == SN_AST_TYPE);
+	
 	switch (node->type) {
 		case SN_AST_LITERAL:
-			asm_mov_id(cgx->buffer, node->children[0], RAX);
+		{
+			ASM(mov_id, IMMEDIATE(node->children[0]), RAX);
 			break;
+		}
+		
 		case SN_AST_SEQUENCE:
+		{
+			SnArray* seq = (SnArray*)node->children[0];
+			ASSERT(seq->base.base.type == SN_ARRAY_TYPE);
+			for (uintx i = 0; i < seq->size; ++i) {
+				codegen_compile_node(cgx, (SnAstNode*)seq->data[i]);
+			}
 			break;
+		}
+		
 		case SN_AST_FUNCTION:
-			asm_mov_id(cgx->buffer, node->children[0], RAX);
+			ASM(mov_id, IMMEDIATE(node->children[0]), RAX);
 			break;
+			
 		case SN_AST_RETURN:
+		{
+			/*
+				Returns require a non-local label, so it is handled specially.
+				The codegen context contains a list (as a linkbuffer) of offsets
+				that want to jump to the return label, so when the body of the
+				function is done, they can all be linked in-place.
+			*/
+			if (node->children[0]) {
+				codegen_compile_node(cgx, (SnAstNode*)node->children[0]);
+			} else {
+				ASM(mov_id, IMMEDIATE(SN_NIL), RAX);
+			}
+			LabelRef ref = ASM(jmp, NULL);
+			if (!cgx->returns) {
+				cgx->returns = snow_create_linkbuffer(16);
+			}
+			snow_linkbuffer_push_data(cgx->returns, (byte*)&ref.offset, sizeof(ref.offset));
+			
+			break;
+		}
+		
 		case SN_AST_BREAK:
+		{
+			TRAP(); // Not implemented yet :(
+		}
+		
 		case SN_AST_CONTINUE:
+		{
+			TRAP(); // Not implemented yet :(
+		}
+		
 		case SN_AST_SELF:
 		case SN_AST_LOCAL:
 		case SN_AST_MEMBER:
