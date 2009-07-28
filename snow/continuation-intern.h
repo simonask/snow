4b8e11195c8f5468b4af28c03b82c70f90007013
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-07-28 14:26:02 +0200

    added __attribute__((hot)) for the continuation functions

--- snow/continuation-intern.h
+++ snow/continuation-intern.h
@@ -4,8 +4,8 @@
 #include "snow/basic.h"
 #include "snow/continuation.h"
 
-HIDDEN __attribute__((noreturn)) void _continuation_resume(SnContinuation* cc);
-HIDDEN __attribute__((returns_twice)) bool _continuation_save(SnContinuation* cc);
-HIDDEN void _continuation_reset(SnContinuation* cc);
+HIDDEN __attribute__((noreturn)) void _continuation_resume(SnContinuation* cc)           ATTR_HOT;
+HIDDEN __attribute__((returns_twice)) bool _continuation_save(SnContinuation* cc)        ATTR_HOT;
+HIDDEN void _continuation_reset(SnContinuation* cc)                                      ATTR_HOT;
 
 #endif /* end of include guard: CONTINUATION_INTERN_H_MHL4KUQW */
--- snow/continuation.h
+++ snow/continuation.h
@@ -36,9 +36,9 @@ CAPI void snow_init_current_continuation();
 CAPI SnContinuation* snow_get_current_continuation();
 
 CAPI SnContinuation* snow_create_continuation(SnFunctionPtr, SnContext* context);
-CAPI VALUE snow_continuation_call(SnContinuation*, SnContinuation* return_to);
-CAPI void snow_continuation_yield(SnContinuation* cc, VALUE val);
-CAPI void snow_continuation_return(SnContinuation* cc, VALUE val);
-CAPI void snow_continuation_resume(SnContinuation* cc);
+CAPI VALUE snow_continuation_call(SnContinuation*, SnContinuation* return_to)        ATTR_HOT;
+CAPI void snow_continuation_yield(SnContinuation* cc, VALUE val)                     ATTR_HOT;
+CAPI void snow_continuation_return(SnContinuation* cc, VALUE val)                    ATTR_HOT;
+CAPI void snow_continuation_resume(SnContinuation* cc)                               ATTR_HOT;
 
 #endif /* end of include guard: CONTINUATION_H_C8DBOWFC */
