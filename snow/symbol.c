e863392d63667367f8f99c45d379c370feaa528a
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-07-28 12:43:14 +0200

    fixed symbol implementation to use SnArray and SnString internally.

--- snow/symbol.c
+++ snow/symbol.c
@@ -1,36 +1,51 @@
 #include "snow/symbol.h"
 #include "snow/intern.h"
+#include "snow/snow.h"
+#include "snow/str.h"
+#include "snow/array.h"
 #include <string.h>
 
-typedef struct Symbol {
-	const char* str;
-} Symbol;
+static VALUE symbol_storage_key = NULL;
 
-static Symbol* symbol_table = NULL;
-static uintx symbol_table_size = 0;
+static SnArray* symbol_storage() {
+	if (!symbol_storage_key)
+		symbol_storage_key = snow_store_add(snow_create_array_with_size(1024));
+	return (SnArray*)snow_store_get(symbol_storage_key);
+}
 
-SnSymbol snow_symbol(const char* str)
+SnSymbol snow_symbol(const char* cstr)
 {
-	uintx i;
-	for (i = 0; i < symbol_table_size; ++i) {
-		if (strcmp(str, symbol_table[i].str) == 0) {
+	// TODO: Mutex lock
+	
+	SnArray* storage = symbol_storage();
+	ASSERT(storage);
+	ASSERT_TYPE(storage, SN_ARRAY_TYPE);
+	
+	SnString* str = snow_create_string(cstr);
+	
+	for (uintx i = 0; i < storage->size; ++i) {
+		ASSERT(is_object(storage->data[i]));
+		SnString* existing = storage->data[i];
+		ASSERT_TYPE(existing, SN_STRING_TYPE);
+		
+		if (snow_string_compare(str, existing) == 0) {
 			return i;
 		}
 	}
 	
-	// TODO: preallocate
-	symbol_table = (Symbol*)realloc(symbol_table, sizeof(Symbol)*(symbol_table_size+1));
-	i = symbol_table_size++;
-	symbol_table[i].str = str;
-	
-	debug("Adding symbol: \"%s\" (%d)\n", str, i);
-	return i;
+	SnSymbol new_symbol = storage->size;
+	snow_array_push(storage, str);
+	return new_symbol;
 }
 
 static const char* symbol_to_string(SnSymbol sym)
 {
-	ASSERT(sym < symbol_table_size);
-	return symbol_table[sym].str;
+	SnArray* storage = symbol_storage();
+	ASSERT(storage);
+	ASSERT(sym < storage->size);
+	SnString* str = snow_array_get(storage, sym);
+	ASSERT(str != SN_NIL);
+	return str->str;
 }
 
 SnObject* create_symbol_prototype()
