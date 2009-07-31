cd4cf83ce9d861afe4fcd5dfd50ef8e2a8204c2d
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-07-31 10:11:41 +0200

    class: dummy constructor

--- snow/class.c
+++ snow/class.c
@@ -1,4 +1,14 @@
 #include "snow/class.h"
+#include "snow/snow.h"
+
+SnClass* snow_create_class(const char* name)
+{
+	SnClass* kl = (SnClass*)snow_alloc_any_object(SN_CLASS_TYPE, sizeof(SnClass));
+	snow_object_init((SnObject*)kl, snow_get_basic_type(SN_CLASS_TYPE));
+	kl->name = name;
+	kl->instance_prototype = snow_create_object(snow_get_basic_type(SN_OBJECT_TYPE));
+	return kl;
+}
 
 SnObject* create_class_prototype()
 {
--- snow/class.h
+++ snow/class.h
@@ -2,5 +2,16 @@
 #define CLASS_H_4Q8LV9FY
 
 #include "snow/object.h"
+#include "snow/str.h"
+#include "snow/function.h"
+
+typedef struct SnClass {
+	SnObject base;
+	SnString* name;
+	SnObject* instance_prototype;
+} SnClass;
+
+CAPI SnClass* snow_create_class(const char* name);
+CAPI VALUE snow_define_method(SnClass* klass, const char* name, SnFunctionPtr method);	// convenience
 
 #endif /* end of include guard: CLASS_H_4Q8LV9FY */
