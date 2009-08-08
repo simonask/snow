e97101f4ceb3ba396c473af5832d0e28d01b1ae2
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-08-08 12:54:05 +0200

    Nil.inspect, Nil.to_string

--- snow/nil.c
+++ snow/nil.c
@@ -1,6 +1,20 @@
 #include "snow/intern.h"
+#include "snow/str.h"
+
+SNOW_FUNC(nil_to_string)
+{
+	ASSERT(SELF == SN_NIL);
+	return snow_create_string("");
+}
+
+SNOW_FUNC(nil_inspect)
+{
+	ASSERT(SELF == SN_NIL);
+	return snow_create_string("nil");
+}
 
 void init_nil_class(SnClass* klass)
 {
-	
+	snow_define_method(klass, "to_string", nil_to_string);
+	snow_define_method(klass, "inspect", nil_inspect);
 }
