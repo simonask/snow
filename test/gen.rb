ddecc1f72463bebb8b205f7d7f4c7520c6aec15e
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-08-24 18:58:10 +0200

    * turned Snow into a nice, well-behaving POSIX-style GNU-ish software package. * added lib/class.sn.

--- test/gen.rb
+++ test/gen.rb
@@ -8,7 +8,7 @@ targets = Dir.glob(File.join(File.dirname(__FILE__), "*.c")).
 all_sources = ["test.c"]
 
 $o.puts "SUBDIRS = ../snow"
-$o.puts "bin_PROGRAMS = #{targets.join(' ')}"
+$o.puts "noinst_PROGRAMS = #{targets.join(' ')}"
 targets.each do |target|
   $o.puts "#{target}_SOURCES = #{target}.c #{all_sources.join(' ')}"
   $o.puts "#{target}_LDADD = ../snow/libsnow.la"
