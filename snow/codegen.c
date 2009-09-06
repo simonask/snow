0192bcc69d1ef41fb5e6cdc4783f592641a878c4
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-09-06 11:21:45 +0200

    * Workaround for Mac OS X 10.6 Snow Leopard.
    mprotect() on Mac OS X 10.6 appears to have a bug where it will touch
    access masks on pages other than the designated one, causing corruption-
    like behavior, but it turns out that malloc()/free() simply lose access
    rights to their internal data structures in some cases.
    
    This workaround pads the allocation with one page on each side.

--- snow/codegen.c
+++ snow/codegen.c
@@ -2,6 +2,10 @@
 #include "snow/codegen-intern.h"
 #include "snow/linkbuffer.h"
 #include "snow/intern.h"
+#include <limits.h>
+#ifndef PAGESIZE
+#define PAGESIZE 4096
+#endif
 
 #include <sys/mman.h>
 
@@ -12,7 +16,8 @@ SnCodegen* snow_create_codegen(SnAstNode* root)
 
 SnFunction* snow_codegen_compile(SnCodegen* cg)
 {
-	return snow_create_function_from_description(snow_codegen_compile_description(cg));
+	SnFunctionDescription* desc = snow_codegen_compile_description(cg);
+	return snow_create_function_from_description(desc);
 }
 
 SnFunctionDescription* snow_codegen_compile_description(SnCodegen* cg)
@@ -25,9 +30,13 @@ SnFunctionDescription* snow_codegen_compile_description(SnCodegen* cg)
 	uintx len = snow_linkbuffer_size(cg->buffer);
 	
 	// FIXME: Use an executable allocator so we can have as many functions as possible in one page of memory.
-	byte* compiled_code = (byte*)valloc(len);
+	// FIXME: (Snow Leopard fix) There seems to be a bug in mprotect that causes pages other than the allocated
+	// ones to be marked as PROT_EXEC, and thus read-only. The following allocated 2 pages of padding on each
+	// side of the allocation, to counter this overflow.
+	byte* compiled_code = (byte*)snow_malloc_aligned(len+2*PAGESIZE, PAGESIZE) + PAGESIZE;
 	snow_linkbuffer_copy_data(cg->buffer, compiled_code, len);
-	mprotect(compiled_code, len, PROT_EXEC);
+	int r = mprotect(compiled_code, len, PROT_EXEC);
+	ASSERT(r == 0);
 	
 	cg->result->func = (SnFunctionPtr)compiled_code;
 	
