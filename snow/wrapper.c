204dbbe792c1c0e678b5509fa188155133ca5626
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-11-25 22:49:15 +0100

    SnWrapper (pointer wrapper) implementation

--- snow/wrapper.c
+++ snow/wrapper.c
@@ -1,16 +1,20 @@
 #include "snow/wrapper.h"
 #include "snow/intern.h"
 
-SnWrapper* snow_create_wrapper(void* ptr)
+static void wrapper_free(VALUE val)
 {
-	SnWrapper* wrapper = (SnWrapper*)snow_alloc_any_object(SN_WRAPPER_TYPE, sizeof(SnWrapper));
-	wrapper->ptr = ptr;
-	return wrapper;
+	ASSERT_TYPE(val, SN_WRAPPER_TYPE);
+	SnWrapper* wrapper = (SnWrapper*)val;
+	wrapper->free_func(wrapper->ptr);
 }
 
-void* snow_wrapper_get_pointer(SnWrapper* wrapper)
+SnWrapper* snow_create_wrapper(void* ptr, SnWrapperFreeFunc free_func)
 {
-	return wrapper->ptr;
+	SnWrapper* wrapper = (SnWrapper*)snow_alloc_any_object(SN_WRAPPER_TYPE, sizeof(SnWrapper));
+	snow_gc_set_free_func(wrapper, wrapper_free);
+	wrapper->ptr = ptr;
+	wrapper->free_func = free_func;
+	return wrapper;
 }
 
 void init_wrapper_class(SnClass* klass)
--- snow/wrapper.h
+++ snow/wrapper.h
@@ -3,12 +3,17 @@
 
 #include "snow/object.h"
 
+// TODO: Optimize this API to require less allocating
+
+typedef void(*SnWrapperFreeFunc)(void* ptr);
+
 typedef struct SnWrapper {
 	SnObjectBase base;
 	void* ptr;
+	SnWrapperFreeFunc free_func;
 } SnWrapper;
 
-CAPI SnWrapper* snow_create_wrapper(void* ptr);
-CAPI void* snow_wrapper_get_pointer(SnWrapper*);
+CAPI SnWrapper* snow_create_wrapper(void* ptr, SnWrapperFreeFunc free_func);
+inline void* snow_wrapper_get_pointer(SnWrapper* wrapper) { return wrapper->ptr; }
 
 #endif /* end of include guard: WRAPPER_H_4DAR1ROT */
