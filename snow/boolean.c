a5cb3d658d6b87e57c5a2d4cdae483721e496f04
Simon Ask Ulsnes <simon@ulsnes.dk>
2009-12-06 17:04:16 +0100

    added Boolean#to_string/inspect

--- snow/boolean.c
+++ snow/boolean.c
@@ -1,6 +1,14 @@
 #include "snow/intern.h"
 
+SNOW_FUNC(boolean_to_string) {
+	if (snow_eval_truth(SELF)) {
+		return snow_create_string("true");
+	}
+	return snow_create_string("false");
+}
+
 void init_boolean_class(SnClass* klass)
 {
-	
+	snow_define_method(klass, "to_string", boolean_to_string);
+	snow_define_method(klass, "inspect", boolean_to_string);
 }
