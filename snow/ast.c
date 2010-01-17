6ba8da39c007a540c863f436bec08d2cd451022d
Mikael Høilund <mikael@hoilund.org>
2010-01-18 00:49:27 +0100

    Added parser support for catch conditions an parameter.

--- snow/ast.c
+++ snow/ast.c
@@ -22,6 +22,7 @@ static uintx ast_size[] = {
 	2, //SN_AST_CALL,
 	2, //SN_AST_LOOP,
 	3, //SN_AST_TRY,
+	3, //SN_AST_CATCH,
 	2, //SN_AST_AND,
 	2, //SN_AST_OR,
 	2, //SN_AST_XOR,
@@ -98,6 +99,7 @@ SnAstNode* snow_ast_call(SnAstNode* func, SnAstNode* seq_args) {
 }
 SnAstNode* snow_ast_loop(SnAstNode* while_true, SnAstNode* body) { return create_ast_node(SN_AST_LOOP, while_true, body); }
 SnAstNode* snow_ast_try(SnAstNode* body, SnAstNode* catch, SnAstNode* ensure) { return create_ast_node(SN_AST_TRY, body, catch, ensure); }
+SnAstNode* snow_ast_catch(SnAstNode* parameter, SnAstNode* condition, SnAstNode* body) { return create_ast_node(SN_AST_CATCH, parameter, condition, body); }
 SnAstNode* snow_ast_and(SnAstNode* left, SnAstNode* right) { return create_ast_node(SN_AST_AND, left, right); }
 SnAstNode* snow_ast_or(SnAstNode* left, SnAstNode* right)  { return create_ast_node(SN_AST_OR, left, right); }
 SnAstNode* snow_ast_xor(SnAstNode* left, SnAstNode* right) { return create_ast_node(SN_AST_XOR, left, right); }
